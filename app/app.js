52d0f476c2676b92b5265b71e71bdad5cfd34bc1
Jakub Chochołowicz <j.chocholowicz@gmail.com>
2023-04-18 20:08:56 +0200

    Lab6

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ import config from './config';
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
+import routes from './REST/routes';
 
 // Instancja serwera
 const app = express();
@@ -16,6 +17,7 @@ app.use(bodyParser.json({limit: '2048kb'}));
 // cors - mechanizm umożliwiający współdzielenie zasobów pomiędzy serwerami znajdującymi się w różnych domenach
 app.use(cors());
 
+routes(app);
 
 // uruchomienie serwera
 app.listen(config.port, function () {
